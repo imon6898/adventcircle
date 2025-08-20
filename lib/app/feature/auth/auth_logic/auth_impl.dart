2133fe5c3f7002fbff1da526fb0bb62fb6abbd5b
Imam Hossain <ihossain6898@gmail.com>
2025-08-20 16:33:18 +0600

    first commit

---
+++ lib/app/feature/auth/auth_logic/auth_impl.dart
@@ -0,0 +1,19 @@
+import 'package:tests/app/feature/auth/auth_logic/auth_api_service.dart';
+import 'package:tests/app/feature/home/home_logic/home_api_service.dart';
+import 'package:tests/app/services/domain/api_service.dart';
+
+class AuthImpl extends AuthApiService {
+
+  @override
+  Future postSignin(String url, Map<String, dynamic> params) async {
+    dynamic response = await ApiService().post(url, params);
+    return response;
+  }
+
+  @override
+  Future postRegistration(String url, Map<String, dynamic> params) async {
+    dynamic response = await ApiService().post(url, params);
+    return response;
+  }
+
+}
